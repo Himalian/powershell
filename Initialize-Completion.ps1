5e8b9b68dc48289f0e180d06afd07fcc19d65399
Himalian <Voltage15312@outlook.com>
2026-07-28 19:15:59 +0800

    feat: setup environment variables and shell completions

---
+++ Initialize-Completion.ps1
@@ -0,0 +1,3 @@
+Invoke-Expression (jj util completion power-shell | Out-String )
+Invoke-Expression( chezmoi completion powershell | Out-String )
+Invoke-Expression( gh completion -s powershell | Out-String )
--- Set-Environment.ps1
+++ Set-Environment.ps1
@@ -1,5 +1,7 @@
 # Set environment variables
-$env:HTTPS_PROXY = "127.0.0.1:2080"
+$env:HTTPS_PROXY = "http://127.0.0.1:2080"
 $env:EDITOR = "nvim"
 $env:LANG = "zh_CN.UTF-8"
 $env:SHELL = "pwsh -nologo"
+$env:BW_PASSWORD = (age --decrypt -i ~/.age/main ~/.age/bw_password.age)
+

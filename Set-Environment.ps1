cb347feb3285f8a79ec0de1cb3066d5d2d698307
Himalian <Voltage15312@outlook.com>
2026-09-11 16:38:43 +0800

    chore: move secrets location

--- Set-Environment.ps1
+++ Set-Environment.ps1
@@ -2,6 +2,6 @@
 $env:HTTPS_PROXY = "http://127.0.0.1:2080"
 $env:EDITOR = "nvim"
 $env:LANG = "zh_CN.UTF-8"
-$env:SHELL = "pwsh -nologo"
-$env:BW_PASSWORD = (age --decrypt -i ~/.age/main ~/.age/bw_password.age)
+$env:SHELL = "pwsh"
+$env:BW_PASSWORD = (age --decrypt -i ~/.age/main ~/.secrets/bw_password.age)
 
